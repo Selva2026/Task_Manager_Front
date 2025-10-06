391a36a7898b2623c0c834c6e88a266bfdefe684
Muthupandi Thamaraiselvam <selvapandi2014@gmail.com>
2025-10-06 12:29:16 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import { store } from "./store/store.js";
+import { store } from "./Store/store.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
 );
+
